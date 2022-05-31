e348c60a0898c2ea71df13691143d83de58057b0
EITARA <kiluna25@gmail.com>
2022-05-31 15:44:28 +0300

    git commit : add map; change settings;

--- app/src/main/java/ru/mirea/lomako/mireaproject/SettingsFragment.java
+++ app/src/main/java/ru/mirea/lomako/mireaproject/SettingsFragment.java
@@ -1,5 +1,9 @@
 package ru.mirea.lomako.mireaproject;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +11,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +23,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class SettingsFragment extends Fragment {
-
+    private EditText path;
+    private EditText historyprofile;
+    private Button save;
+    private SharedPreferences preferences;
+    final String SAVED_Sett_Path = "saved_sett_path";
+    final String SAVED_Sett_Profile = "saved_sett_profile";
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +73,34 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings, null);
+        path = v.findViewById(R.id.path);
+        historyprofile = v.findViewById(R.id.historyprofile);
+        save = v.findViewById(R.id.SettSave);
+        preferences = getActivity().getPreferences(MODE_PRIVATE);
+        onLoadText(v);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSaveText(v);
+            }
+        });
+        return v;
+        }
+    public void onSaveText(View view) {
+        SharedPreferences.Editor editor = preferences.edit();
+        // Сохранение значения по ключу SAVED_TEXT
+        editor.putString(SAVED_Sett_Path, path.getText().toString());
+        editor.putString(SAVED_Sett_Profile, historyprofile.getText().toString());
+        editor.apply();
+        Toast.makeText(getActivity(), "Settings Saved", Toast.LENGTH_SHORT).show();
     }
-}
+    public void onLoadText(View view) {
+        // Загрузка значения по ключу SAVED_TEXT
+        String text = preferences.getString(SAVED_Sett_Path, "Название папки");
+        String text2 = preferences.getString(SAVED_Sett_Profile, "Название профиля");
+        path.setText(text);
+        historyprofile.setText(text2);
+    }
+
+    }
