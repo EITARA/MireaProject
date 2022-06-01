27dd18e72998b1447c73691843a97d1c48f59cdb
EITARA <kiluna25@gmail.com>
2022-06-01 22:33:58 +0300

    create login firebase

--- app/src/main/java/ru/mirea/lomako/mireaproject/ui/home/HomeFragment.java
+++ app/src/main/java/ru/mirea/lomako/mireaproject/ui/home/HomeFragment.java
@@ -1,19 +1,26 @@
 package ru.mirea.lomako.mireaproject.ui.home;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import ru.mirea.lomako.mireaproject.Login;
+import ru.mirea.lomako.mireaproject.R;
 import ru.mirea.lomako.mireaproject.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
+    private FirebaseAuth mAuth;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -26,6 +33,20 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button logout = binding.btnSignOut;
+        mAuth = FirebaseAuth.getInstance();
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int i = view.getId();
+                if (i == R.id.btnSignOut) {
+                    mAuth.signOut();
+                    Intent intent = new Intent(getActivity(), Login.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
         return root;
     }
 
